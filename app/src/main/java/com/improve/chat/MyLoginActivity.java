8b7e8fa5f05707eacddace33a29d589412094594
Vladimir Kvachevskii <pro@itremedy.pro>
2023-03-14 18:38:28 +0400

    transfer to gitlab

---
+++ app/src/main/java/com/improve/chat/MyLoginActivity.java
@@ -0,0 +1,119 @@
+package com.improve.chat;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MyLoginActivity extends AppCompatActivity {
+
+    private TextInputEditText editTextEmail, editTextPassword;
+    private Button buttonSignin, buttonSignup;
+    private TextView textViewForgot;
+
+    FirebaseAuth auth;
+    FirebaseUser firebaseUser;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (firebaseUser != null)
+        {
+            Intent intent = new Intent(MyLoginActivity.this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_login);
+
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonSignin = findViewById(R.id.buttonSignIn);
+        buttonSignup = findViewById(R.id.buttonSignUp);
+        textViewForgot = findViewById(R.id.textViewForgot);
+
+        auth = FirebaseAuth.getInstance();
+
+
+        buttonSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = editTextEmail.getText().toString();
+                String password = editTextPassword.getText().toString();
+
+                if (!email.equals("") && !password.equals(""))
+                {
+                    signin(email,password);
+                }
+                else
+                {
+                    Toast.makeText(MyLoginActivity.this, "Write your credentials"
+                            , Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        buttonSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MyLoginActivity.this,SignUpActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
+        textViewForgot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(MyLoginActivity.this,ForgetActivity.class);
+                startActivity(i);
+                finish();
+
+            }
+        });
+
+    }
+
+    public void signin(String email, String password)
+    {
+        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful())
+                {
+                    Intent intent = new Intent(MyLoginActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                }
+                else
+                {
+                    Toast.makeText(MyLoginActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/improve/chat/SignUpActivity.java
@@ -0,0 +1,178 @@
+package com.improve.chat;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.util.UUID;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private CircleImageView circleImageView;
+    private TextInputEditText editTextEmailSignup, editTextPasswordSignup, editTextUsernameSignup;
+    private Button buttonRegister;
+    Boolean imageControl = false;
+
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+    DatabaseReference reference;
+
+    FirebaseStorage firebaseStorage;
+    StorageReference storageReference;
+    Uri imageUri;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        circleImageView = findViewById(R.id.circleImageView);
+        editTextEmailSignup = findViewById(R.id.editTextEmailSignUp);
+        editTextPasswordSignup = findViewById(R.id.editTextPasswordSignUp);
+        editTextUsernameSignup = findViewById(R.id.editTextUsernameSignup);
+        buttonRegister = findViewById(R.id.buttonRegister);
+
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference();
+        firebaseStorage = FirebaseStorage.getInstance();
+        storageReference = firebaseStorage.getReference();
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = editTextEmailSignup.getText().toString();
+                String password = editTextPasswordSignup.getText().toString();
+                String userName = editTextUsernameSignup.getText().toString();
+
+                if (!email.equals("") && !password.equals("") && !userName.equals(""))
+                {
+                    signup(email,password,userName);
+                }
+                else
+                {
+                    Toast.makeText(SignUpActivity.this, "Write something in the fields", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+
+        circleImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                imageChooser();
+
+            }
+        });
+    }
+
+    public  void imageChooser()
+    {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null)
+        {
+            imageUri = data.getData();
+            Picasso.get().load(imageUri).into(circleImageView);
+            imageControl = true;
+        }
+        else
+        {
+            imageControl = false;
+        }
+    }
+
+    public void signup(String email, String password, String userName)
+    {
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful())
+                {
+                    reference.child("Users").child(auth.getUid()).child("userName").setValue(userName);
+
+                    if (imageControl)
+                    {
+                        UUID randomID = UUID.randomUUID();
+                        String imageName = "images/" + randomID + ".jpg";
+                        storageReference.child(imageName).putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                                StorageReference myStorageRef = firebaseStorage.getReference(imageName);
+                                myStorageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                    @Override
+                                    public void onSuccess(Uri uri) {
+                                        String filePath = uri.toString();
+                                        reference.child("Users").child(auth.getUid()).child("image").setValue(filePath).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void unused) {
+                                                Toast.makeText(SignUpActivity.this, "Write to database is successful", Toast.LENGTH_SHORT).show();
+                                            }
+                                        }).addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Toast.makeText(SignUpActivity.this, "Write to database is not successful", Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+                                    }
+                                });
+
+                            }
+                        });
+                    }
+                    else
+                    {
+                        reference.child("Users").child(auth.getUid()).child("image").setValue("null");
+                    }
+
+                    Intent intent = new Intent(SignUpActivity.this,MainActivity.class);
+
+                    startActivity(intent);
+                    finish();
+                }
+                else
+                {
+                    Toast.makeText(SignUpActivity.this, "There is a problem", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+}
